1fb412fc6869f8366e4f6492758e7cc377886914
Guilherme Peixe <guilherme@softable.com.br>
2021-04-02 22:17:51 -0300

    feat: ensure SignUpController returns 400 if no password is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,10 +1,10 @@
-import { badRequest } from '../helpers/http'
+import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredParameters = ['name', 'email']
+    const requiredParameters = ['name', 'email', 'password']
     for (const parameter of requiredParameters) {
       if (!httpRequest.body[parameter]) {
         return badRequest(new Error())

92ed9e2662286a419db8e10a09b532643421f71f
Guilherme Peixe <guilherme@softable.com.br>
2021-04-08 20:47:40 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -1,22 +1,26 @@
-import { HttpRequest, HttpResponse, Controller, badRequest, MissingParamError, InvalidParamError, EmailValidator } from './signup-controller-protocols'
+import { HttpRequest, HttpResponse, Controller, badRequest, MissingParamError, InvalidParamError, EmailValidator, serverError } from './signup-controller-protocols'
 
 export class SignUpController implements Controller {
-  constructor (private readonly emailValidator: EmailValidator) {}
+  constructor (private readonly emailValidator: EmailValidator) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredParameters = ['name', 'email', 'password', 'passwordConfirmation']
-    for (const parameter of requiredParameters) {
-      if (!httpRequest.body[parameter]) {
-        return badRequest(new MissingParamError(parameter))
+    try {
+      const requiredParameters = ['name', 'email', 'password', 'passwordConfirmation']
+      for (const parameter of requiredParameters) {
+        if (!httpRequest.body[parameter]) {
+          return badRequest(new MissingParamError(parameter))
+        }
       }
-    }
-    const { password, passwordConfirmation, email } = httpRequest.body
-    if (password !== passwordConfirmation) {
-      return badRequest(new InvalidParamError('passwordConfirmation'))
-    }
-    const isValid = this.emailValidator.isValid(email)
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'))
+      const { password, passwordConfirmation, email } = httpRequest.body
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
